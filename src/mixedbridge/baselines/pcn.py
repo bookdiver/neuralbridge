Fix the guided proposal method, now it should work properly, some primitive trials on optimizing the kl divergence, not working now.

src/mixedbridge/baselines/pcn.py
@@ -2,16 +2,21 @@ import jax
 import jax.numpy as jnp
 from functools import partial
 
+from ..stochastic_processes.bases import ContinuousTimeProcess
 from ..stochastic_processes.gbp import GuidedBridgeProcess
 from ..stochastic_processes.wiener import WienerProcess
 from ..solvers.sde import Euler
 
 class PreconditionedCrankNicolson:
     def __init__(self, 
-                 rho: float,
                  x0: jnp.ndarray,
-                 guided_process: GuidedBridgeProcess,
+                 xT: jnp.ndarray,
+                 ori_proc: ContinuousTimeProcess,
+                 aux_proc: ContinuousTimeProcess,
                  *,
+                 rho: float,
+                 L0: jnp.ndarray,
+                 Sigma0: jnp.ndarray,
                  rng_key: jax.Array = jax.random.PRNGKey(42),
                  n_batches: int = 1
                 ):
@@ -19,11 +24,11 @@ class PreconditionedCrankNicolson:
         self.x0 = x0
         self.n_batches = n_batches
         
-        self.dim = guided_process.dim
-        self.dtype = guided_process.dtype
-        self.T = guided_process.T
-        self.dt = guided_process.dt
-        self.ts = guided_process.ts
+        self.dim = ori_proc.dim
+        self.dtype = ori_proc.dtype
+        self.T = ori_proc.T
+        self.dt = ori_proc.dt
+        self.ts = ori_proc.ts
 
         self.wiener = WienerProcess(self.dim, self.dtype, self.T, self.dt)
         self.rng_key = rng_key
@@ -31,10 +36,11 @@ class PreconditionedCrankNicolson:
         self.W_sampler = self.wiener.path_sampler(key1, n_batches=self.n_batches)
         self.Z_sampler = self.wiener.path_sampler(key2, n_batches=self.n_batches)
 
+        guided_process = GuidedBridgeProcess(ori_proc, aux_proc, L0, Sigma0, x0=x0, xT=xT)
         self.solver = Euler(guided_process, self.wiener)
 
     @partial(jax.jit, static_argnums=(0,))
-    def update_step(self, rng_key, path_X, ll_X, Ws):
+    def run_step(self, rng_key, path_X, ll_X, Ws):
         key1, key2 = jax.random.split(rng_key, 2)
         Zs = self.wiener.sample_path(key1, ts=self.ts, n_batches=self.n_batches).xs
         Wos = self.rho * Ws + jnp.sqrt(1 - self.rho**2) * Zs
@@ -64,18 +70,18 @@ class PreconditionedCrankNicolson:
         return path_X_new, Ws_new, ll_X_new, accepted
 
     @partial(jax.jit, static_argnums=(0, 1))
-    def update_loop(self, n_iters, init_state):
+    def run_loop(self, n_iters, init_state):
         def body_fun(i, state):
             path_X, Ws, ll_X, accepts, ll_Xs, rng_key = state
             rng_key, subkey = jax.random.split(rng_key)
-            path_X, Ws, ll_X, accepted = self.update_step(subkey, path_X, ll_X, Ws)
+            path_X, Ws, ll_X, accepted = self.run_step(subkey, path_X, ll_X, Ws)
             accepts += accepted
             ll_Xs = ll_Xs.at[i+1].set(ll_X)
             return path_X, Ws, ll_X, accepts, ll_Xs, rng_key
 
         return jax.lax.fori_loop(0, n_iters, body_fun, init_state)
 
-    def update(self, n_iters: int, verbose: bool=True, log_every: int=1):
+    def run(self, n_iters: int, verbose: bool=True, log_every: int=1):
         key1, key2 = jax.random.split(self.rng_key)
         Ws = self.wiener.sample_path(key1, ts=self.ts, n_batches=self.n_batches).xs
         path_X = self.solver.solve(self.x0, rng_key=None, dWs=Ws, log_likelihood=True, n_batches=self.n_batches)
@@ -84,7 +90,7 @@ class PreconditionedCrankNicolson:
         ll_Xs = ll_Xs.at[0].set(ll_X)
 
         init_state = (path_X, Ws, ll_X, jnp.zeros(self.n_batches), ll_Xs, key2)
-        path_X, Ws, ll_X, accepts, ll_Xs, self.rng_key = self.update_loop(n_iters, init_state)
+        path_X, Ws, ll_X, accepts, ll_Xs, self.rng_key = self.run_loop(n_iters, init_state)
 
         if verbose:
             for i in range(0, n_iters, log_every):

src/mixedbridge/solvers/ode.py
@@ -12,24 +12,24 @@ SolverState = namedtuple("SolverState", ["L", "M_diag", "mu"])
 class BackwardODE:
     def __init__(
         self,
-        original_process: ContinuousTimeProcess,
-        auxiliary_process: AuxiliaryProcess,
+        ori_proc: ContinuousTimeProcess,
+        aux_proc: AuxiliaryProcess,
         L0: jnp.ndarray,
         Sigma0: jnp.ndarray,
     ):
-        self.g = partial(original_process.g, x=None)
-        self.B = auxiliary_process.B
-        self.beta = auxiliary_process.beta
+        self.Sigma = partial(ori_proc.Sigma, x=None)
+        self.B = aux_proc.B
+        self.beta = aux_proc.beta
         self.L0 = L0
         self.Sigma0 = Sigma0
 
     def solve_step(self, solver_state: namedtuple, t: float, dt: float) -> namedtuple:
-        L = kernel_r3(lambda t, x: -self.B(t) @ x, t, solver_state.L, dt)
+        L = kernel_r3(lambda t, x: x @ self.B(t), t, solver_state.L, dt)
         M_diag = kernel_r3(
-            lambda t, x: L @ self.g(t) @ self.g(t).T @ L.T, t, solver_state.M_diag, dt
+            lambda t, x: L @ self.Sigma(t) @ L.T, t, solver_state.M_diag, dt
         )
         M = jnp.linalg.inv(M_diag)
-        mu = kernel_r3(lambda t, x: -L @ self.beta(t), t, solver_state.mu, dt)
+        mu = kernel_r3(lambda t, x: L @ self.beta(t), t, solver_state.mu, dt)
 
         new_state = SolverState(L=L, M_diag=M_diag, mu=mu)
         return new_state, (L, M, mu)
@@ -43,6 +43,7 @@ class BackwardODE:
         _, (Ls, Ms, mus) = jax.lax.scan(solve_step, init_state, reverse_ts)
 
         return Ls[::-1, ...], Ms[::-1, ...], mus[::-1, ...]
+        # return Ls, Ms, mus
 
 
 def kernel_r3(

src/mixedbridge/solvers/sde.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from functools import partial
 import abc
 import jax
 import jax.numpy as jnp
@@ -16,6 +17,7 @@ class SDESolver(abc.ABC):
     ):
         self.sde = sde
         self.wiener_process = wiener_process
+        self._dts = jnp.diff(self.sde.ts)  # Cache dts
 
     @property
     def ts(self):
@@ -23,7 +25,7 @@ class SDESolver(abc.ABC):
 
     @property
     def dts(self):
-        return jnp.diff(self.ts)
+        return self._dts
 
     @abc.abstractmethod
     def step(
@@ -32,20 +34,9 @@ class SDESolver(abc.ABC):
         pass
 
     def solve(
-        self, x0: jnp.ndarray, rng_key: jax.Array, dWs: jnp.ndarray, *, log_likelihood: bool = False, path: SamplePath = None, n_batches: int = 1
+        self, x0: jnp.ndarray, rng_key: jax.Array, dWs: jnp.ndarray | None, *, log_likelihood: bool = False, n_batches: int = 1
     ) -> SamplePath:
-        if dWs is None:
-            assert rng_key is not None, "Either dWs or rng_key must be provided"
-            path_sampler = self.wiener_process.path_sampler(
-                rng_key,
-                evaluate_ts=self.ts,
-                n_batches=n_batches
-            )
-            dWs = next(path_sampler).xs
-        else:
-            assert dWs.shape[0] == n_batches, "Number of batches must match the shape of dWs"
-            
-
+        
         def scan_fn(carry: tuple, val: tuple) -> tuple:
             x, log_psi = carry
             t, dt, dW = val
@@ -53,6 +44,16 @@ class SDESolver(abc.ABC):
             G = self.sde.G(t, x) if log_likelihood else 0.0
             log_psi += G * dt
             return (x_next, log_psi), x_next
+        
+        if dWs is None:
+            assert rng_key is not None, "Either dWs or rng_key must be provided"
+            dWs = self.wiener_process.sample_path(
+                rng_key,
+                ts=self.ts,
+                n_batches=n_batches
+            ).xs
+        else:
+            assert dWs.shape[0] == n_batches, "Number of batches must match the shape of dWs"
 
         (_, log_ll), xs = jax.vmap(
             lambda dW: jax.lax.scan(
@@ -61,15 +62,10 @@ class SDESolver(abc.ABC):
             in_axes=0
         )(dWs)
 
-        if path:
-            path.xs = xs
-            path.ts = self.ts[1:]
-            path.log_likelihood = log_ll
-            return path
-        else:
-            return SamplePath(xs=xs, ts=self.ts[1:], log_likelihood=log_ll)
+        return SamplePath(xs=xs, ts=self.ts[1:], log_likelihood=log_ll)
 
 
 class Euler(SDESolver):
+    @partial(jax.jit, static_argnums=(0,))
     def step(self, x: jnp.ndarray, t: float, dt: float, dW: jnp.ndarray):
         return x + self.sde.f(t, x) * dt + self.sde.g(t, x) @ dW

src/mixedbridge/stochastic_processes/bases.py
@@ -1,16 +1,38 @@
 import abc
-from functools import partial
 
-import jax
 import jax.numpy as jnp
 
 class ContinuousTimeProcess(abc.ABC):
+    """
+    Abstract base class for continuous-time stochastic processes defined by SDEs:
+    dX(t) = f(t, X(t))dt + g(t, X(t))dW(t)
+
+    This class provides the structure for stochastic processes used in modeling.
+    It defines abstract methods for key components of the process and implements
+    some concrete methods based on these abstractions.
+
+    Attributes:
+        dim (int): The dimension of the process.
+        dtype (jnp.dtype): The data type used for numerical computations.
+        T (float): The total time span of the process.
+        dt (float): The time step size.
+    """
+
     dim: int
     dtype: jnp.dtype
     T: float
     dt: float
     
     def __init__(self, T: float, dt: float, dim: int, dtype: jnp.dtype):
+        """
+        Initialize the ContinuousTimeProcess.
+
+        Args:
+            T (float): The total time span of the process.
+            dt (float): The time step size.
+            dim (int): The dimension of the process.
+            dtype (jnp.dtype): The data type for numerical computations.
+        """
         self.T = T
         self.dt = dt
         self.dim = dim
@@ -18,60 +40,351 @@ class ContinuousTimeProcess(abc.ABC):
 
     @property
     def ts(self):
+        """
+        Returns the time grid.
+
+        Returns:
+            jnp.ndarray: An array representing the time grid.
+        """
         assert self.dt is not None, "Time step size must be provided!"
         return jnp.arange(0, self.T + self.dt, self.dt, dtype=self.dtype)
     
     @property
     def reverse_ts(self):
+        """
+        Returns the reversed time grid.
+
+        Returns:
+            jnp.ndarray: An array representing the reversed time grid.
+        """
         return jnp.flip(self.ts) 
     
     @property
     def dts(self):
+        """
+        Returns the time step differences.
+
+        Returns:
+            jnp.ndarray: An array of time step differences.
+        """
         return jnp.diff(self.ts) 
     
     @abc.abstractmethod
-    @partial(jax.jit, static_argnums=(0,))
     def f(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the drift function.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The drift value at time t and state x.
+        """
         pass
     
     @abc.abstractmethod
-    @partial(jax.jit, static_argnums=(0,))
     def g(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the diffusion function.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The diffusion value at time t and state x.
+        """
         pass
 
     @abc.abstractmethod
-    @partial(jax.jit, static_argnums=(0,))
     def Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the covariance matrix.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The covariance matrix at time t and state x.
+        """
         pass
     
+    def inv_Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Returns the inverse covariance matrix.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The inverse of the covariance matrix at time t and state x.
+        """
+        return jnp.linalg.inv(self.Sigma(t, x, *args, **kwargs))
+    
 class AuxiliaryProcess(ContinuousTimeProcess):
+    """
+    Represents an auxiliary process in continuous time, inheriting from ContinuousTimeProcess.
+    Auxiliary process is defined by the following SDE:
+    dX_tilde(t) = (beta(t) + B(t)*X_tilde)dt + g(t)dW(t)
+    
+    This class defines the structure for auxiliary processes used in stochastic modeling.
+    It provides abstract methods for key components of the process and implements
+    some concrete methods based on these abstractions.
+
+    Attributes:
+        dim (int): The dimension of the process.
+        dtype (jnp.dtype): The data type used for numerical computations.
+        T (float): The total time span of the process.
+        dt (float): The time step size.
+    """
+
     dim: int
     dtype: jnp.dtype
     T: float
     dt: float
     
     def __init__(self, T: float, dt: float, dim: int, dtype: jnp.dtype):
+        """
+        Initialize the AuxiliaryProcess.
+
+        Args:
+            T (float): The total time span of the process.
+            dt (float): The time step size.
+            dim (int): The dimension of the process.
+            dtype (jnp.dtype): The data type for numerical computations.
+        """
         super().__init__(T, dt, dim, dtype)
 
     @abc.abstractmethod
     def beta(self, t: float, *args, **kwargs):
+        """
+        Abstract method for the beta function of the process.
+
+        Args:
+            t (float): The current time.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The beta value at time t.
+        """
         pass
 
     @abc.abstractmethod
     def B(self, t: float, *args, **kwargs):
+        """
+        Abstract method for the B function of the process.
+
+        Args:
+            t (float): The current time.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The B value at time t.
+        """
         pass
 
     def f(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Compute the drift function of the process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            jnp.ndarray: The drift value at time t and state x.
+        """
         return self.beta(t, *args, **kwargs) + self.B(t, *args, **kwargs) @ x
 
     @abc.abstractmethod
     def g(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the diffusion function of the process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The diffusion value at time t and state x.
+        """
         pass 
 
     @abc.abstractmethod
     def Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the covariance matrix of the process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The covariance matrix at time t and state x.
+        """
         pass
     
+    def inv_Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Compute the inverse of the covariance matrix.
 
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            jnp.ndarray: The inverse of the covariance matrix at time t and state x.
+        """
+        return jnp.linalg.inv(self.Sigma(t, x, *args, **kwargs))
+
+class BridgeProcess(ContinuousTimeProcess):
+    """
+    Abstract base class for bridge processes.
+    Bridge process is defined by the following SDE:
+    dX^*(t) = (f(t, X^*(t)) + Sigma(t, X^*(t)) * score(t, X^*(t)))dt + g(t, X^*(t))dW(t)
+
+    A bridge process is a stochastic process conditioned to start at a given initial state
+    and end at a specified final state at time T.
+
+    Attributes:
+        dim (int): The dimension of the process.
+        dtype (jnp.dtype): The data type of the process.
+        T (float): The final time of the process.
+        dt (float): The time step for discretization.
+        xT (jnp.ndarray): The final state of the process.
+    """
+
+    dim: int
+    dtype: jnp.dtype
+    T: float
+    dt: float
+    xT: jnp.ndarray
     
+    def __init__(self, T: float, dt: float, dim: int, dtype: jnp.dtype, xT: jnp.ndarray):
+        """
+        Initialize the BridgeProcess.
 
+        Args:
+            T (float): The final time of the process.
+            dt (float): The time step for discretization.
+            dim (int): The dimension of the process.
+            dtype (jnp.dtype): The data type of the process.
+            xT (jnp.ndarray): The final state of the process.
+        """
+        super().__init__(T, dt, dim, dtype)
+        self.xT = xT
+        
+    @abc.abstractmethod
+    def score(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the score function of the process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The score value at time t and state x.
+        """
+        pass
+    
+    @abc.abstractmethod
+    def _f(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the drift function of the unconditioned process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The drift value of the unconditioned process at time t and state x.
+        """
+        pass
+    
+    def f(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Compute the drift function of the bridge process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The drift value of the bridge process at time t and state x.
+        """
+        return self._f(t, x, *args, **kwargs) + jnp.dot(self.Sigma(t, x, *args, **kwargs), self.score(t, x, *args, **kwargs))
+    
+    @abc.abstractmethod
+    def g(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the diffusion function of the process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The diffusion value at time t and state x.
+        """
+        pass
+    
+    @abc.abstractmethod
+    def Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Abstract method for the covariance matrix of the process.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            The covariance matrix at time t and state x.
+        """
+        pass
+    
+    def inv_Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        """
+        Compute the inverse of the covariance matrix.
+
+        Args:
+            t (float): The current time.
+            x (jnp.ndarray): The current state.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            jnp.ndarray: The inverse of the covariance matrix at time t and state x.
+        """
+        return jnp.linalg.inv(self.Sigma(t, x, *args, **kwargs))

src/mixedbridge/stochastic_processes/examples.py
@@ -27,6 +27,9 @@ class OUProcess(ContinuousTimeProcess):
     def Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
         return self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
     
+    def inv_Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
+        return 1.0 / self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
+    
 class OUBridgeProcess(ContinuousTimeProcess):
     
     def __init__(self,
@@ -53,6 +56,9 @@ class OUBridgeProcess(ContinuousTimeProcess):
     
     def Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
         return self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
+    
+    def inv_Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
+        return 1.0 / self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
 
 
 class CellDiffusionProcess(ContinuousTimeProcess):
@@ -83,7 +89,9 @@ class CellDiffusionProcess(ContinuousTimeProcess):
     def Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
         return self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
     
-
+    def inv_Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
+        return 1.0 / self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
+    
 class CellDiffusionAuxProcess(AuxiliaryProcess):
 
     def __init__(self, 

src/mixedbridge/stochastic_processes/gbp.py
@@ -1,34 +1,34 @@
 from functools import partial
+from typing import Callable
 import jax
 import jax.numpy as jnp
 
 from .bases import ContinuousTimeProcess, AuxiliaryProcess
 from ..solvers.ode import BackwardODE
-from ..utils.sample_path import SamplePath
 
 class GuidedBridgeProcess(ContinuousTimeProcess):
 
     def __init__(self, 
-                 original_process: ContinuousTimeProcess, 
-                 auxiliary_process: AuxiliaryProcess,
+                 ori_proc: ContinuousTimeProcess, 
+                 aux_proc: AuxiliaryProcess,
                  L0: jnp.ndarray,
                  Sigma0: jnp.ndarray,
                  *,
-                 start: jnp.ndarray,
-                 target: jnp.ndarray,
+                 x0: jnp.ndarray,
+                 xT: jnp.ndarray,
                  ):
-        super().__init__(original_process.T, original_process.dt, original_process.dim, original_process.dtype)
-        assert start.shape == target.shape
+        super().__init__(ori_proc.T, ori_proc.dt, ori_proc.dim, ori_proc.dtype)
+        assert x0.shape == xT.shape
         
-        self.ori_process = original_process
-        self.aux_process = auxiliary_process
+        self.ori_proc = ori_proc
+        self.aux_proc = aux_proc
 
-        self.start = start
-        self.target = target
+        self.x0 = x0
+        self.xT = xT
 
         self.backward_ode = BackwardODE(
-            original_process, 
-            auxiliary_process, 
+            ori_proc, 
+            aux_proc, 
             L0, 
             Sigma0=Sigma0)
 
@@ -42,17 +42,20 @@ class GuidedBridgeProcess(ContinuousTimeProcess):
     def r(self, t: float, x: jnp.ndarray) -> jnp.ndarray:
         t_idx = self.find_t(t)
         L, M, mu = self.Ls[t_idx], self.Ms[t_idx], self.mus[t_idx]
-        return -L.T @ self.aux_process.B(t) @ M @ (self.target - mu - L @ x)
+        return L.T @ M @ (self.xT - mu - L @ x)
     
     def f(self, t: float, x: jnp.ndarray, *args, **kwargs):
-        return self.ori_process.f(t, x, *args, *kwargs)  \
+        return self.ori_proc.f(t, x, *args, *kwargs)  \
                + self.Sigma(t, x, *args, **kwargs) @ self.r(t, x, *args, **kwargs)
     
     def g(self, t: float, x: jnp.ndarray, *args, **kwargs):
-        return self.ori_process.g(t, x, *args, **kwargs)
+        return self.ori_proc.g(t, x, *args, **kwargs)
 
     def Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
-        return self.g(t, x, *args, **kwargs) @ self.g(t, x, *args, **kwargs).T
+        return self.ori_proc.Sigma(t, x, *args, **kwargs)
+    
+    def inv_Sigma(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        return self.ori_proc.inv_Sigma(t, x, *args, **kwargs)
     
     def solve_backward_ode(self):
         Ls, Ms, mus = self.backward_ode.solve(self.reverse_ts)
@@ -60,30 +63,27 @@ class GuidedBridgeProcess(ContinuousTimeProcess):
     
     def G(self, t: float, x: jnp.ndarray) -> float:
         r = self.r(t, x)
-        term1 = (self.ori_process.f(t, x) - self.aux_process.f(t, x)) @ r
-        # term2 = 0.5 * jnp.trace(
-        #     (self.ori_process.Sigma(t, x) - self.aux_process.Sigma(t, x))
-        #     @ (self.Hs[self.find_t(t)] - r @ r.T)
-        # )
-        A = self.ori_process.Sigma(t, x) - self.aux_process.Sigma(t, x)
-        term2 = -0.5 * jnp.trace(A @ self.Hs[self.find_t(t)])
-        term3 = 0.5 * r @ A @ r
-        return term1 + term2 + term3
+        term1 = jnp.inner(self.ori_proc.f(t, x) - self.aux_proc.f(t, x), r)
+        A = self.ori_proc.Sigma(t, x) - self.aux_proc.Sigma(t, x)
+        term2 = -0.5 * jnp.trace(A @ (self.Hs[self.find_t(t)] - r @ r.T))
+        return term1 + term2
+
+class MixedGuidedBridgeProcess(GuidedBridgeProcess):
     
-    def log_likelihood(self, sample_path: SamplePath, skip: int = 25) -> float:
-        ts, xs = sample_path.ts, sample_path.xs
-        res = 0.0
+    def __init__(self, 
+                 ori_proc: ContinuousTimeProcess, 
+                 aux_proc: AuxiliaryProcess,
+                 L0: jnp.ndarray,
+                 Sigma0: jnp.ndarray,
+                 *,
+                 x0: jnp.ndarray,
+                 xT: jnp.ndarray,
+                 nu: Callable[[jnp.ndarray, jnp.ndarray], jnp.ndarray],
+                 ):
+        super().__init__(ori_proc=ori_proc, aux_proc=aux_proc, L0=L0, Sigma0=Sigma0, x0=x0, xT=xT)
+        self.nu = nu
 
-        def scan_fn(carry: tuple, val: tuple) -> tuple:
-            log_psi = carry
-            t, t_next, x = val
-            G = self.G(t, x)
-            log_psi += G * (t_next - t)
-
-            return log_psi, (t, t_next, x)
-
-        slice1 = slice(None, -(skip+1))
-        slice2 = slice(1, -skip) if skip > 0 else slice(1, None) 
-        res, _ = jax.lax.scan(scan_fn, 0.0, (ts[slice1], ts[slice2], xs[slice1])) 
-        
-        return res
+    def f(self, t: float, x: jnp.ndarray, *args, **kwargs):
+        return self.ori_proc.f(t, x, *args, *kwargs)  \
+               + self.Sigma(t, x, *args, **kwargs) @ self.r(t, x, *args, **kwargs) \
+               + self.g(t, x, *args, **kwargs) @ self.nu(t, x, *args, **kwargs)

src/mixedbridge/stochastic_processes/wiener.py
@@ -26,6 +26,9 @@ class WienerProcess(ContinuousTimeProcess):
     
     def Sigma(self, t: float, x: jnp.ndarray) -> jnp.ndarray:
         pass
+    
+    def inv_Sigma(self, t: float, x: jnp.ndarray) -> jnp.ndarray:
+        pass
 
     def sample_step(self, dt: float, rng_key: jax.Array) -> jnp.ndarray:
         dW = jax.random.normal(rng_key, (self.dim,), dtype=self.dtype) * jnp.sqrt(dt)
@@ -50,11 +53,19 @@ class WienerProcess(ContinuousTimeProcess):
             name=f"{self.dim}-dimensional Wiener process"
         )
     
-    def path_sampler(self, rng_key: jax.Array, evaluate_ts: jnp.ndarray = None, *, n_batches: int = 1) -> Generator[SamplePath, None, None]:
-        if evaluate_ts is None:
-            ts = self.ts
-        else:
-            ts = evaluate_ts
+    def path_sampler(self, rng_key: jax.Array, evaluate_ts: jnp.ndarray | None = None, *, n_batches: int = 1) -> Generator[SamplePath, None, None]:
+        """
+        Generates a sample path of the Wiener process, used for the MCMC sampling.
+
+        Args:
+            rng_key (jax.Array): Random number generator key.
+            evaluate_ts (jnp.ndarray, optional): Time steps at which to evaluate the process. Defaults to None.
+            n_batches (int, optional): Number of batches to generate. Defaults to 1.
+
+        Yields:
+            Generator[SamplePath, None, None]: A generator yielding sample paths of the Wiener process.
+        """
+        ts = evaluate_ts if evaluate_ts is not None else self.ts
             
         while True:
             rng_key, subkey = jax.random.split(rng_key)