Initial commit for the new branch for developing the MCMC+NN training scheme, including some solver backprop performance optimization, MCMC fixes, and new examples

neuralbridge/models/neurb.py
@@ -13,18 +13,16 @@ from neuralbridge.utils.sample_path import SamplePath
 
 @struct.dataclass
 class TrainState(train_state.TrainState):
-    batch_stats: dict = struct.field(pytree_node=True)
     rng_key: jax.Array = struct.field(pytree_node=True)
 
     @classmethod
-    def create(cls, *, apply_fn, params, tx, batch_stats, rng_key, **kwargs):
+    def create(cls, *, apply_fn, params, tx, rng_key, **kwargs):
         return cls(
             step=0,
             apply_fn=apply_fn,
             params=params,
             tx=tx,
             opt_state=tx.init(params),
-            batch_stats=batch_stats,
             rng_key=rng_key,
             **kwargs,
         )
@@ -63,11 +61,9 @@ class NeuralBridge:
             self.init_rng_key,
             dummy_t,
             dummy_x,
-            training=False
         )
         
         self.init_params = variables["params"]
-        self.init_batch_stats = variables["batch_stats"] if "batch_stats" in variables else {}
         self.state = None 
     
     def _initialize_optimizer(self, learning_rate: Optional[float] = None) -> None:
@@ -102,7 +98,6 @@ class NeuralBridge:
             apply_fn=self.neural_net.apply,
             params=self.init_params if self.state is None else self.state.params,
             tx=optimizer,
-            batch_stats=self.init_batch_stats if self.state is None else self.state.batch_stats,
             rng_key=self.init_rng_key if self.state is None else self.state.rng_key,
         )
     
@@ -114,50 +109,44 @@ class NeuralBridge:
         )
     
     @partial(jax.jit, static_argnums=(0))
-    def _compute_loss(self, params, batch_stats, state, dWs: jnp.ndarray, u: jnp.ndarray, v: jnp.ndarray) -> Tuple[jnp.ndarray, dict]:
+    def _compute_loss(self, params, dWs: jnp.ndarray, u: jnp.ndarray, v: jnp.ndarray) -> Tuple[jnp.ndarray, dict]:
         path = self.path_solver.solve_with_variables(
             x0=u,
             variables={
                 "params": params,
-                "batch_stats": batch_stats
             },
             dWs=dWs,
             batch_size=self.batch_size,
             enforce_end_point=v,
-            training=True,
-            mutable=["batch_stats"]
         )
-        ts, xs, log_ll = path.ts, path.xs, path.log_ll
-        ts = repeat(ts, "t -> b t 1", b=self.batch_size)
+        log_ll, nus = path.log_ll, path.nus
+        # ts, xs, log_ll = path.ts, path.xs, path.log_ll
+        # ts = repeat(ts, "t -> b t 1", b=self.batch_size)
         
-        ts_flatten = rearrange(ts[:, 1:, :], "b t 1 -> (b t) 1")
-        xs_flatten = rearrange(xs[:, 1:, :], "b t d -> (b t) d")  
-        nus, updated_batch_stats = state.apply_fn(
-            variables = {
-                "params": params,
-                "batch_stats": batch_stats
-            },
-            t=ts_flatten,
-            x=xs_flatten,
-            training=True,
-            mutable=["batch_stats"]
-        )
-        nus = rearrange(nus, "(b t) d -> b t d", b=self.batch_size)
+        # ts_flatten = rearrange(ts[:, 1:, :], "b t 1 -> (b t) 1")
+        # xs_flatten = rearrange(xs[:, 1:, :], "b t d -> (b t) d")  
+        # nus, updated_batch_stats = state.apply_fn(
+        #     variables = {
+        #         "params": params,
+        #     },
+        #     t=ts_flatten,
+        #     x=xs_flatten,
+        # )
+        # nus = rearrange(nus, "(b t) d -> b t d", b=self.batch_size)
         loss = jnp.sum(0.5 * jnp.sum(nus ** 2, axis=-1), axis=1) * self.path_solver.dt - log_ll # (b, )
         loss = jnp.mean(loss, axis=0)
-        return loss, updated_batch_stats
+        return loss
     
     @partial(jax.jit, static_argnums=(0))
     def _train_step(self, state, dWs: jnp.ndarray, u: jnp.ndarray, v: jnp.ndarray) -> Tuple[TrainState, jnp.ndarray]:
         
         def loss_fn(params):
-            loss, updated_batch_stats = self._compute_loss(params, state.batch_stats, state, dWs, u, v)
-            return loss, updated_batch_stats
+            loss = self._compute_loss(params, dWs, u, v)
+            return loss
         
-        (loss, aux), grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+        loss, grads = jax.value_and_grad(loss_fn, has_aux=False)(state.params)
         
         state = state.apply_gradients(grads=grads)
-        state = state.replace(batch_stats=aux["batch_stats"])
         state = state.replace(rng_key=jax.random.split(state.rng_key)[0])
         return state, loss
     
@@ -212,7 +201,6 @@ class NeuralBridge:
                 ckpt_dir=absolute_save_dir,
                 target={
                     "params": self.state.params,
-                    "batch_stats": self.state.batch_stats,
                     "opt_state": self.state.opt_state,
                     "rng_key": self.state.rng_key,
                     "epoch": epoch,
@@ -244,7 +232,6 @@ class NeuralBridge:
         self.state = TrainState(
             apply_fn=self.neural_net.apply,
             params=ckpt["params"],
-            batch_stats=ckpt["batch_stats"],
             tx=self.state.tx,
             opt_state=ckpt["opt_state"],
             rng_key=ckpt["rng_key"],
@@ -259,12 +246,9 @@ class NeuralBridge:
             x0=x0,
             variables={
                 "params": self.state.params,
-                "batch_stats": self.state.batch_stats
             },
             dWs=dWs,
             batch_size=batch_size,
             enforce_end_point=enforce_end_point,
-            training=False,
-            mutable=False
         )
         return path

neuralbridge/networks/mlps.py
@@ -22,20 +22,15 @@ def get_activation(activation: str) -> Callable:
 class MLPSmall(nn.Module):
     out_dim: int
     hidden_dims: Sequence[int]
-    norm: Optional[str] = None
     activation: Optional[str] = "tanh"
     dtype: Optional[jnp.dtype] = jnp.float32
     
     @nn.compact
-    def __call__(self, t: jnp.ndarray, x: jnp.ndarray, training: bool = True):
+    def __call__(self, t: jnp.ndarray, x: jnp.ndarray):
         
         x = jnp.concatenate([t, x], axis=-1)
         for hidden_dim in self.hidden_dims:
             x = nn.Dense(hidden_dim, dtype=self.dtype)(x)
-            if self.norm == "batch":
-                x = nn.BatchNorm(use_running_average=not training)(x)
-            elif self.norm == "layer":
-                x = nn.LayerNorm()(x)
             x = get_activation(self.activation)(x)
         x = nn.Dense(self.out_dim, dtype=self.dtype)(x)
         return x
@@ -43,7 +38,6 @@ class MLPSmall(nn.Module):
 class MLP(nn.Module):
     out_dim: int
     hidden_dims: Sequence[int]
-    norm: Optional[str] = None
     activation: Optional[str] = "tanh"
     t_emb_dim: int = 32
     t_emb_max_period: float = 100.0
@@ -51,7 +45,7 @@ class MLP(nn.Module):
     dtype: Optional[jnp.dtype] = jnp.float32
     
     @nn.compact
-    def __call__(self, t: jnp.ndarray, x: jnp.ndarray, training: bool = True):
+    def __call__(self, t: jnp.ndarray, x: jnp.ndarray):
         t_emb = TimeEmbedding(
             self.t_emb_dim,
             self.t_emb_max_period,
@@ -64,10 +58,6 @@ class MLP(nn.Module):
         
         for hidden_dim in self.hidden_dims:
             x = nn.Dense(hidden_dim, dtype=self.dtype)(x)
-            if self.norm == "batch":
-                x = nn.BatchNorm(use_running_average=not training)(x)
-            elif self.norm == "layer":
-                x = nn.LayerNorm()(x)
             x = get_activation(self.activation)(x)
         x = nn.Dense(self.out_dim, dtype=self.dtype)(x)
         return x

neuralbridge/solvers/sde.py
@@ -111,7 +111,6 @@ class SDESolver(abc.ABC):
     @partial(jax.grad, argnums=5)
     def step_with_variables(
         self, x: jnp.ndarray, t: float, dt: float, dW: jnp.ndarray, variables: Any,
-        *, training: bool = False, mutable: Optional[Union[str, Tuple[str, ...], bool]] = False
     ):
         pass
     
@@ -188,9 +187,6 @@ class SDESolver(abc.ABC):
         dWs: jnp.ndarray,
         batch_size: Optional[int] = 1,
         enforce_end_point: Optional[jnp.ndarray] = None,
-        *,
-        training: bool = False,
-        mutable: Optional[Union[List[str], bool]] = False,
     ) -> SamplePath:
         if hasattr(self.sde, "G"):
             record_ll = True
@@ -199,14 +195,12 @@ class SDESolver(abc.ABC):
 
         def scan_fn(solver_state: namedtuple, t_W_args: tuple) -> tuple:
             t, dt, dW = t_W_args
-            x = self.step_with_variables(
+            x, nu = self.step_with_variables(
                 x=solver_state.x, 
                 t=t, 
                 dt=dt, 
                 dW=dW,
                 variables=variables,
-                training=training,
-                mutable=mutable
             )
             G = self.sde.G(t, x) if record_ll else 0.0
             log_ll = solver_state.log_ll + G * dt
@@ -214,12 +208,12 @@ class SDESolver(abc.ABC):
                 x=x, 
                 log_ll=log_ll, 
             )
-            return new_state, x
+            return new_state, (x, nu)
         
         assert dWs.shape[0] == batch_size, "Number of batches must match the shape of dWs"
 
         init_state = SolverState(x=x0, log_ll=0.0)
-        final_state, xs = jax.vmap(
+        final_state, (xs, nus) = jax.vmap(
             lambda dW: jax.lax.scan(
                 scan_fn, 
                 init=init_state, 
@@ -240,7 +234,8 @@ class SDESolver(abc.ABC):
             ts=self.ts,
             dWs=dWs,
             dts=self.dts,
-            log_ll=final_state.log_ll
+            log_ll=final_state.log_ll,
+            nus=nus,
         )
 
 
@@ -252,12 +247,11 @@ class Euler(SDESolver):
         return x + drift * dt + diffusion @ dW
     
     def step_with_variables(
-        self, x: jnp.ndarray, t: float, dt: float, dW: jnp.ndarray, variables: Any,
-        *, training: bool = False, mutable: Optional[Union[List[str], bool]] = None
-    ) -> jnp.ndarray:
-        drift = self.sde.f(t, x, variables, training=training, mutable=mutable)
+        self, x: jnp.ndarray, t: float, dt: float, dW: jnp.ndarray, variables: Any
+    ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        drift, nu = self.sde.f(t, x, variables)
         diffusion = self.sde.g(t, x)
-        return x + drift * dt + diffusion @ dW
+        return x + drift * dt + diffusion @ dW, nu
     
 # class ModifiedEuler(SDESolver):
 #     # !!! NOTE: May not work now

neuralbridge/stochastic_processes/conds.py
@@ -112,7 +112,7 @@ class GuidedBridgeProcess(ContinuousTimeProcess):
         r = self.r(t, x)
         term1 = jnp.inner(self.ori_proc.f(t, x) - self.aux_proc.f(t, x), r)
         A = self.ori_proc.Sigma(t, x) - self.aux_proc.Sigma(t, x)
-        term2 = -0.5 * jnp.trace(A @ (self.Hs[self._find_t(t)] - r @ r.T))
+        term2 = -0.5 * jnp.trace(A @ (self.Hs[self._find_t(t)] -jnp.outer(r, r)))
         return term1 + term2
 
 class NeuralBridgeProcess(ContinuousTimeProcess):
@@ -127,17 +127,14 @@ class NeuralBridgeProcess(ContinuousTimeProcess):
         self.neural_net = neural_net
         
     def nu(
-        self, t: float, x: jnp.ndarray, variables: Any,
-        *, training: bool = False, mutable: Optional[Union[str, List[str], bool]] = False
+        self, t: float, x: jnp.ndarray, variables: Any
     ) -> jnp.ndarray:
         t = rearrange(jnp.array([t]), "t -> 1 t")
         x = rearrange(x, "d -> 1 d")
-        output, *_ = self.neural_net.apply(
+        output = self.neural_net.apply(
             variables=variables,
-            t=t, 
-            x=x, 
-            training=training,
-            mutable=mutable
+            t=t,
+            x=x
         )
         if output.ndim == 1:
             return output
@@ -145,11 +142,10 @@ class NeuralBridgeProcess(ContinuousTimeProcess):
             return rearrange(output, "1 d -> d")
 
     def f(
-        self, t: float, x: jnp.ndarray, variables: Any,
-        training: bool = False, mutable: Optional[Union[str, List[str], bool]] = False
-    ) -> jnp.ndarray:
-        return self.guided_process.f(t, x)  \
-               + self.g(t, x) @ self.nu(t, x, variables, training=training, mutable=mutable)
+        self, t: float, x: jnp.ndarray, variables: Any
+    ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        nu = self.nu(t, x, variables)
+        return self.guided_process.f(t, x) + self.g(t, x) @ nu, nu
                
     def g(self, t: float, x: jnp.ndarray):
         return self.guided_process.g(t, x)

neuralbridge/stochastic_processes/examples.py
@@ -210,18 +210,17 @@ class CellDiffusionAuxProcess(AuxiliaryProcess):
         self.alpha = alpha
         self.sigma = sigma
 
-    def beta(self, t: jnp.ndarray, *args, **kwargs):
+    def beta(self, t: jnp.ndarray):
         return jnp.ones(self.dim, dtype=self.dtype)
 
-    def B(self, t: jnp.ndarray, *args, **kwargs):
+    def B(self, t: jnp.ndarray):
         return - 1.0 * jnp.eye(self.dim, dtype=self.dtype)
 
-    def g(self, t: jnp.ndarray, x: jnp.ndarray, *args, **kwargs):
+    def g(self, t: jnp.ndarray, x: jnp.ndarray):
         return self.sigma * jnp.eye(self.dim, dtype=self.dtype)
 
-    def Sigma(self, t: jnp.ndarray, x: jnp.ndarray, *args, **kwargs):
+    def Sigma(self, t: jnp.ndarray, x: jnp.ndarray):
         return self.sigma**2 * jnp.eye(self.dim, dtype=self.dtype)
-
 
 class LandmarkLagrangianProcess(ContinuousTimeProcess):
     
@@ -240,10 +239,10 @@ class LandmarkLagrangianProcess(ContinuousTimeProcess):
         dim = int(n_landmarks * m_landmarks) if dim is None else dim
         super().__init__(T, dim, dtype)
     
-    def f(self, t: jnp.ndarray, x: jnp.ndarray, *args, **kwargs) -> jnp.ndarray:
+    def f(self, t: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
         return jnp.zeros(self.dim, dtype=self.dtype)
     
-    def g(self, t: jnp.ndarray, x: jnp.ndarray, *args, **kwargs) -> jnp.ndarray:
+    def g(self, t: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
         x_landmarks = rearrange(x, "(n m) -> n m", n=self.n_landmarks, m=self.m_landmarks)
         kernel_fn = lambda dist_mat: 0.5 * self.k_alpha * jnp.exp(-jnp.sum(dist_mat**2, axis=-1) / (2.0 * self.k_sigma**2))
         dist_mat = x_landmarks[:, None, :] - x_landmarks[None, :, :]
@@ -279,12 +278,79 @@ class LandmarkLagrangianAuxProcess(AuxiliaryProcess):
         Q0_half = rearrange(Q0_half, "i k j l -> (i k) (j l)")
         self.g0 = Q0_half
         
-    def beta(self, t: jnp.ndarray, *args, **kwargs):
+    def beta(self, t: jnp.ndarray):
         return jnp.zeros(self.dim, dtype=self.dtype)
     
-    def B(self, t: jnp.ndarray, *args, **kwargs):
+    def B(self, t: jnp.ndarray):
         return jnp.zeros((self.dim, self.dim), dtype=self.dtype)
     
-    def g(self, t: jnp.ndarray, x: jnp.ndarray, *args, **kwargs):
+    def g(self, t: jnp.ndarray, x: jnp.ndarray):
         assert self.g0 is not None, "g0 must be initialized using .init_g(x0)"
         return self.g0
+
+class FitzHughNagumoProcess(ContinuousTimeProcess):
+    
+    def __init__(self,
+                 epsilon: float,
+                 s: float,
+                 gamma: float,
+                 b: float,
+                 sigma: float,
+                 T: Optional[float] = 10.0,
+                 dim: Optional[int] = 2,
+                 dtype: Optional[jnp.dtype] = jnp.float32):
+        super().__init__(T, dim, dtype)
+        self.epsilon = epsilon
+        self.s = s
+        self.gamma = gamma
+        self.b = b
+        self.sigma = sigma
+        
+    def f(self, t: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
+        a = jnp.array([[1. / self.epsilon, -1. / self.epsilon],
+                       [self.gamma,        -1.]], 
+                      dtype=self.dtype)
+        b = jnp.array([- x[0]**3 / self.epsilon + self.s / self.epsilon,
+                       self.b], 
+                      dtype=self.dtype)
+        return a @ x + b
+    
+    def g(self, t: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.array([[0., 0.],
+                          [0., self.sigma]], 
+                         dtype=self.dtype)
+        
+class FitzHughNagumoAuxProcess(AuxiliaryProcess):
+    
+    def __init__(self, 
+                 epsilon: float,
+                 s: float,
+                 gamma: float,
+                 b: float,
+                 sigma: float,
+                 nu: float,
+                 T: Optional[float] = 10.0,
+                 dim: Optional[int] = 2,
+                 dtype: Optional[jnp.dtype] = jnp.float32):
+        super().__init__(T, dim, dtype)
+        self.epsilon = epsilon
+        self.s = s
+        self.gamma = gamma
+        self.b = b
+        self.sigma = sigma
+        self.nu = nu
+        
+    def B(self, t: jnp.ndarray) -> jnp.ndarray:
+        return jnp.array([[1. / self.epsilon - 3. * self.nu**2 / self.epsilon, -1. / self.epsilon],
+                          [self.gamma,                                         -1.]], 
+                         dtype=self.dtype)
+        
+    def beta(self, t: jnp.ndarray) -> jnp.ndarray:
+        return jnp.array([2. * self.nu**3 / self.epsilon + self.s / self.epsilon,
+                          self.b], 
+                         dtype=self.dtype)
+        
+    def g(self, t: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.array([[0., 0.],
+                          [0., self.sigma]], 
+                         dtype=self.dtype)